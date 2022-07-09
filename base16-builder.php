8a4462d862fd37ba80919c9cff81d7f7935fd27c
Chris Kempson <git@chriskempson.com>
2022-07-09 22:18:16 +0100

    A few tweaks

--- base16-builder.php
+++ base16-builder.php
@@ -12,28 +12,30 @@ use Mexitek\PHPColors\Color;
 // Read standard input
 $scheme_file = ''; 
 while (FALSE !== ($line = fgets(STDIN))) { $scheme_file .= $line; } 
+fclose(STDIN);
 
 // Handle command line arguments
 $options = getopt('t:', ['template:']);
 $template_file = $options['t'] ?? $options['template'] ?? null;
-if (empty($template_file)) die('Specify template with -t or --template=');
+if (empty($template_file)) { fwrite(STDERR, 'Specify template with -t or --template='); exit; }
 
 // Parse Scheme YAML
 $scheme_data = Yaml::parse($scheme_file);
 
 // Create scheme and template data
-$template_data = buildDataForTemplate($scheme_data);
+$template_data = buildTagsForTemplate($scheme_data);
 
 // Render parsed template
-echo renderTemplate($template_file, $template_data);
+fwrite(STDOUT, renderTemplate($template_file, $template_data));
 
 /* 
- * Create variables to be used in mustache templates
+ * Create variable tags to be used in mustache templates
  */
-function buildDataForTemplate($scheme_data)
+function buildTagsForTemplate($scheme_data)
 {
-    $vars['scheme-name'] = $scheme_data['scheme'];
-    $vars['scheme-author'] = $scheme_data['author'];
+    $tags['scheme-name'] = $scheme_data['scheme'];
+	$tags['scheme-slug'] = str_replace(' ', '-', strtolower($tags['scheme-name']));
+    $tags['scheme-author'] = $scheme_data['author'];
 
     $bases = array('00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
         '0A', '0B', '0C', '0D', '0E', '0F');
@@ -42,33 +44,33 @@ function buildDataForTemplate($scheme_data)
         $base_key = 'base' . $base;
         $color = new Color(str_replace('#', '', $scheme_data[$base_key]));
 
-        $vars[$base_key . '-hex'] = $color->getHex();
-        $vars[$base_key . '-hex-bgr'] = substr($color->getHex(), 4, 2) .
+        $tags[$base_key . '-hex'] = $color->getHex();
+        $tags[$base_key . '-hex-bgr'] = substr($color->getHex(), 4, 2) .
             substr($color->getHex(), 2, 2) . substr($color->getHex(), 0, 2);
-        $vars[$base_key . '-hex-r'] = substr($color->getHex(), 0, 2);
-        $vars[$base_key . '-hex-g'] = substr($color->getHex(), 2, 2);
-        $vars[$base_key . '-hex-b'] = substr($color->getHex(), 4, 2);
-        $vars[$base_key . '-rgb-r'] = $color->getRgb()['R'];
-        $vars[$base_key . '-rgb-g'] = $color->getRgb()['G'];
-        $vars[$base_key . '-rgb-b'] = $color->getRgb()['B'];
-        $vars[$base_key . '-dec-r'] = $color->getRgb()['R'] / 255;
-        $vars[$base_key . '-dec-g'] = $color->getRgb()['G'] / 255;
-        $vars[$base_key . '-dec-b'] = $color->getRgb()['B'] / 255;
-        $vars[$base_key . '-hsl-h'] = $color->getHsl()['H'];
-        $vars[$base_key . '-hsl-s'] = $color->getHsl()['S'];
-        $vars[$base_key . '-hsl-l'] = $color->getHsl()['L'];
+        $tags[$base_key . '-hex-r'] = substr($color->getHex(), 0, 2);
+        $tags[$base_key . '-hex-g'] = substr($color->getHex(), 2, 2);
+        $tags[$base_key . '-hex-b'] = substr($color->getHex(), 4, 2);
+        $tags[$base_key . '-rgb-r'] = $color->getRgb()['R'];
+        $tags[$base_key . '-rgb-g'] = $color->getRgb()['G'];
+        $tags[$base_key . '-rgb-b'] = $color->getRgb()['B'];
+        $tags[$base_key . '-dec-r'] = $color->getRgb()['R'] / 255;
+        $tags[$base_key . '-dec-g'] = $color->getRgb()['G'] / 255;
+        $tags[$base_key . '-dec-b'] = $color->getRgb()['B'] / 255;
+        $tags[$base_key . '-hsl-h'] = $color->getHsl()['H'];
+        $tags[$base_key . '-hsl-s'] = $color->getHsl()['S'];
+        $tags[$base_key . '-hsl-l'] = $color->getHsl()['L'];
     }
 
-    return $vars;
+    return $tags;
 }
 
 /* 
  * Renders a template using Mustache
  */
-function renderTemplate($path, $template_data)
+function renderTemplate($path, $template_tags)
 {
     $mustache = new \Mustache_Engine();
     $mustache = $mustache->loadTemplate(file_get_contents($path));
 
-    return $mustache->render($template_data);
+    return $mustache->render($template_tags);
 }
